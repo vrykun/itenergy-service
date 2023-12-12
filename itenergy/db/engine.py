77d612a285b93b41e506a5ea8f42327071d473af
Vladyslav Rykun <vladyslavrykun@gmail.com>
2023-12-12 02:36:46 +0200

    Set variables for github actions

--- itenergy/db/engine.py
+++ itenergy/db/engine.py
@@ -8,7 +8,7 @@ def get_db_url() -> str:
         os.getenv('POSTGRES_USER', 'itenergy'),
         os.getenv('POSTGRES_PASSWORD', 'password'),
         os.getenv('PGHOST', 'itenergy-service-postgres'),
-        os.getenv('PGPORT', '5432'),
+        os.getenv('PGPORT', 5432),
         os.getenv('POSTGRES_DB', 'itenergy'),
     )
 
